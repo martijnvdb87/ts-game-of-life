0560b2ade1b75d0b38ca2f0cbb7b3558bb8671b3
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2021-08-24 20:53:00 +0200

    Added Board class

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
-import './style.css'
+//import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+class Cell {
+  x: number;
+  y: number;
+  isAlive: boolean = false;
+  isAliveInNextTurn: boolean = false;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  getNeighbours() {
+
+  };
+}
+
+class Board {
+  cells: Array<Array<Cell>> = new Array<Array<Cell>>();
+
+  constructor(width: number, height: number) {
+    this.createBoard(width, height);
+  };
+
+  createBoard(width: number, height: number) {
+    for(let y = 0; y < height; y++) {
+      let row: Array<Cell> = [];
+
+      for(let x = 0; x < width; x++) {
+        let cell: Cell = new Cell(x, y);
+        row.push(cell);
+      }
+
+      this.cells.push(row);
+    }
+  };
+}
+
+let board: Board = new Board(10, 10);
+console.log(board.cells);
