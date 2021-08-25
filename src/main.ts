83fcad1d6626eaddd3c70e047e20def533b4c64e
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2021-08-25 20:55:40 +0200

    Added getNeighbours method

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,36 @@
 //import './style.css'
 
 class Cell {
+  board: Board;
   x: number;
   y: number;
+  neighbours: Array<Cell> = new Array<Cell>();
   isAlive: boolean = false;
   isAliveInNextTurn: boolean = false;
 
-  constructor(x: number, y: number) {
+  constructor(board: Board, x: number, y: number) {
+    this.board = board;
     this.x = x;
     this.y = y;
   }
 
   getNeighbours() {
+    if(this.neighbours.length === 0) {
 
+      for(let y = -1; y < 2; y++) {
+        for(let x = -1; x < 2; x++) {
+          if(y === 0 && x === 0) {
+            continue;
+          }
+
+          if(this.board.cells[this.y + y]?.[this.x + x]) {
+            this.neighbours.push(this.board.cells[this.y + y][this.x + x]);
+          }
+        }
+      }
+    }
+
+    return this.neighbours;
   };
 }
 
@@ -28,7 +46,7 @@ class Board {
       let row: Array<Cell> = [];
 
       for(let x = 0; x < width; x++) {
-        let cell: Cell = new Cell(x, y);
+        let cell: Cell = new Cell(this, x, y);
         row.push(cell);
       }
 
@@ -38,4 +56,4 @@ class Board {
 }
 
 let board: Board = new Board(10, 10);
-console.log(board.cells);
+console.log(board.cells[0][0].getNeighbours());
